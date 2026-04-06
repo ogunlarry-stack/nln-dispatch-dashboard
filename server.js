f94aa4f83d073648f5e0be5896517a49da0f05ed
ogunlarry-stack <ogunlarry@aol.com>
2026-04-05 23:16:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
-const twilio = require("twilio");
-
-const client = twilio(
-"YOUR_ACCOUNT_SID",
-"YOUR_AUTH_TOKEN"
-);
-
 app.post("/api/sms/:id", async (req, res) => {
 const lead = leads.find(l => String(l.id) === String(req.params.id));
 
-if (!lead) {
-return res.status(404).json({ success: false });
-}
+if (!lead) return res.json({ success: false });
 
 try {
 await client.messages.create({
@@ -20,7 +11,7 @@ Location: ${lead.location}
 Customer: ${lead.name}
 Phone: ${lead.phone}`,
 from: "+18336964193",
-to: "+YOUR_VERIFIED_NUMBER"
+to: "+14435781686"
 });
 
 console.log("REAL SMS SENT");
@@ -28,7 +19,7 @@ console.log("REAL SMS SENT");
 res.json({ success: true });
 
 } catch (err) {
-console.log(err);
+console.log("SMS ERROR:", err);
 res.json({ success: false });
 }
 });
